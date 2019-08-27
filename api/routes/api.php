d6b15fdd48e690cc986e5ad540b5d0e427196e69
HaelioMarcio <haelioferreira@yahoo.com.br>
2019-08-27 19:54:51 -0300

    Criação das Rotas, Controller, Model e Migration Eventos;

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resources(['usuarios' => 'UsuariosController']);
+Route::resources(['eventos' => 'EventosController']);
