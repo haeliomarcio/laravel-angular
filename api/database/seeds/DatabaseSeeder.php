db83c7a6ce6fd7dd0ed18aee7ffe56f1ce283729
HaelioMarcio <haelioferreira@yahoo.com.br>
2019-08-27 20:02:42 -0300

    Configuracao do ENV e Criação do Seed do Usuário:

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Usuario;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        Usuario::create({
+            'name' => 'Administrador do Sistema',
+            'email' => 'administrador@sistema.com.br',
+            'birthday' => '1995-04-27',
+            'phone' => '8588887777',
+            'password' => encrypt('Imts@2019');
+        });
     }
 }
