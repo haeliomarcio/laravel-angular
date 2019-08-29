44c34828e059331442734aca95bb463653862796
HaelioMarcio <=>
2019-08-28 22:02:13 -0300

    Criacao do Projeto Angular

--- api/app/Http/Middleware/IsLogged.php
+++ api/app/Http/Middleware/IsLogged.php
@@ -3,7 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Redis;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
 class IsLogged
 {
     /**
@@ -15,10 +18,17 @@ class IsLogged
      */
     public function handle($request, Closure $next)
     {
-        //Redis::set('name', 'Taylor');
-        //$values = Redis::lrange('names', 5, 10);
-        //echo $values; die;
-        //print_r($request->header('token')); die;
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'Token is Invalid']);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }else{
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
         return $next($request);
     }
 }
